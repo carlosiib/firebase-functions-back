6b0284f269bb53bf2b815441921792e1e19cdecc
carlosiib <ntic.carlos@gmail.com>
2020-07-30 14:11:06 -0700

    Libros component

--- src/components/Libros.jsx
+++ src/components/Libros.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import { LibrosContext } from "../context/LibrosProvider"
+
 const Libros = () => {
+  const { libros } = React.useContext(LibrosContext)
   return (
-    <div>
-      Libros
+    <div className="mt-5">
+      <h3>Lista de libros</h3>
+      <ul className="list-group">
+        {/* .id, .titulo, son propiedades que vienen de la collecion */}
+        {
+          libros.map(libro => (
+            <li
+              key={libro.id}
+              className="list-group-item">
+              <span> {libro.titulo}</span>
+            </li>
+          )
+          )
+        }
+      </ul>
     </div>
   )
 }
